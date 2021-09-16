a7c76e78055d6bd7be1f86684dee3b465e80fa2e
qqqwwwddd <qqqwwwddd1@gamil.com>
2021-09-16 12:06:57 +0900

    익셉션 처리 완료

--- src/main/java/com/cos/blogapp/web/BoardController.java
+++ src/main/java/com/cos/blogapp/web/BoardController.java
@@ -2,6 +2,7 @@ package com.cos.blogapp.web;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.cos.blogapp.domain.board.Board;
 import com.cos.blogapp.domain.board.BoardRepository;
 import com.cos.blogapp.domain.user.User;
+import com.cos.blogapp.handler.ex.MyNotFoundException;
 import com.cos.blogapp.util.Script;
 import com.cos.blogapp.web.dto.BoardSaveReqDto;
 
@@ -47,7 +49,12 @@ public class BoardController {
 
 		// 2. orElseThrow
 		Board boardEntity = boardRepository.findById(id)
-				.orElseThrow();
+				.orElseThrow(new Supplier<MyNotFoundException>() {
+					@Override
+					public MyNotFoundException get() {
+						return new MyNotFoundException(id+"를 찾을 수 없습니다");
+					}
+				}); 
 			
 			model.addAttribute("boardEntity", boardEntity);
 			return "board/detail";
