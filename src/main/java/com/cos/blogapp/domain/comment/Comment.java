e0bb1da11583febeb7eb4769b5e0c89da1613d88
qqqwwwddd <qqqwwwddd1@gmail.com>
2021-09-30 13:55:56 +0900

    comment 테이블 생성 완료

---
+++ src/main/java/com/cos/blogapp/domain/comment/Comment.java
@@ -0,0 +1,36 @@
+package com.cos.blogapp.domain.comment;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.cos.blogapp.domain.board.Board;
+import com.cos.blogapp.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// 테이블 모델
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Comment {  
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; //PK (자동증가 번호)
+	
+	private String content;
+	
+	@JoinColumn(name = "userId") // FK의 컬럼명 정해줌
+	@ManyToOne
+	private User user;
+	
+	@JoinColumn(name = "boardId") // FK의 컬럼명 정해줌
+	@ManyToOne
+	private Board board;
+}
