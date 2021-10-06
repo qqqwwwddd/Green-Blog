ba3cff005d447d14759f9eb94d15bb67925d917b
qqqwwwddd <qqqwwwddd1@gmail.com>
2021-10-06 09:35:31 +0900

    서비스레이어, 영속성컨텍스트&더티체킹

--- src/main/java/com/cos/blogapp/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cos/blogapp/handler/GlobalExceptionHandler.java
@@ -1,32 +1,29 @@
 package com.cos.blogapp.handler;
-
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.cos.blogapp.handler.ex.MyAsyncNotFoundException;
 import com.cos.blogapp.handler.ex.MyNotFoundException;
 import com.cos.blogapp.util.Script;
 import com.cos.blogapp.web.dto.CMRespDto;
-
-// @ControllerAdvice 이친구는 1. 익셉션 핸들링, 2. @Controller의 역할까지 한다.
+// @ControllerAdvice 이친구는 1. 익셉션 핸들링, 2. @Controller 의 역할까지 한다.
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	// 어떤 익셉션은 파일 
-	// 어떤 익셉션은 데이터
-	// 어떤 익셉션은 뒤로가기 
-	// 어떤 익셉션은 / 메인페이지로 가게
-	
+	// 어떤 익셉션은 파일~~~
+	// 어떤 익셉션은 데이터~~~
+	// 어떤 익셉션은 뒤로 가기
+	// 어떤 익셉션은 / 메인 페이지로 가게!!
+	// 일반요청
 	@ExceptionHandler(value = MyNotFoundException.class)
-	public @ResponseBody String error1(MyNotFoundException e) { 
-		System.out.println(" 오류 터졌어 :" + e.getMessage());
+	public @ResponseBody String error1(MyNotFoundException e) {
+		System.out.println("오류 터졌어 : " + e.getMessage());
 		return Script.href("/", e.getMessage());
 	}
 
-	
+	// fetch 요청 (데이터를 응답받아야 할 때)
 	@ExceptionHandler(value = MyAsyncNotFoundException.class)
-	public @ResponseBody CMRespDto<String> error2(MyAsyncNotFoundException e) { 
-		System.out.println(" 오류 터졌어 :" + e.getMessage());
-		  return new CMRespDto<String>(-1, e.getMessage(), null);
-	   }
+	public CMRespDto<String> error2(MyAsyncNotFoundException e) {
+		System.out.println("오류 터졌어 : " + e.getMessage());
+		return new CMRespDto<String>(-1, e.getMessage(), null);
+	}
 }
--- src/main/java/com/cos/blogapp/web/UserController.java
+++ src/main/java/com/cos/blogapp/web/UserController.java
@@ -1,13 +1,9 @@
 package com.cos.blogapp.web;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,29 +12,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.cos.blogapp.domain.user.User;
-import com.cos.blogapp.domain.user.UserRepository;
 import com.cos.blogapp.handler.ex.MyAsyncNotFoundException;
-import com.cos.blogapp.util.MyAlgorithm;
-import com.cos.blogapp.util.SHA;
+import com.cos.blogapp.service.UserService;
 import com.cos.blogapp.util.Script;
 import com.cos.blogapp.web.dto.CMRespDto;
 import com.cos.blogapp.web.dto.JoinReqDto;
 import com.cos.blogapp.web.dto.LoginReqDto;
 import com.cos.blogapp.web.dto.UserUpdateDto;
-
 import lombok.RequiredArgsConstructor;
-
 @RequiredArgsConstructor
 @Controller
 public class UserController {
-
-	private final UserRepository userRepository;
+	private final UserService userService;
 	private final HttpSession session;
-
 	@PutMapping("/user/{id}")
-	public @ResponseBody CMRespDto<String> update(@PathVariable int id, @Valid @RequestBody UserUpdateDto dto, BindingResult bindingResult) {
+	public @ResponseBody CMRespDto<String> update(@PathVariable int id, @Valid @RequestBody UserUpdateDto dto,
+			BindingResult bindingResult) {
 		// 유효성
 		if (bindingResult.hasErrors()) {
 			Map<String, String> errorMap = new HashMap<>();
@@ -47,100 +37,72 @@ public class UserController {
 			}
 			throw new MyAsyncNotFoundException(errorMap.toString());
 		}
-		
 		// 인증
 		User principal = (User) session.getAttribute("principal");
 		if (principal == null) { // 로그인 안됨
-			throw new MyAsyncNotFoundException("인증이 되지 않았습니다");
+			throw new MyAsyncNotFoundException("인증이 되지 않았습니다.");
 		}
-		
-		// 권한 - principal get id랑 id 같은지
-		if(principal.getId() != id) {
-			throw new MyAsyncNotFoundException("회원정보를 수정할 권한이 없습니다");
+		// 권한
+		if (principal.getId() != id) {
+			throw new MyAsyncNotFoundException("회원정보를 수정할 권한이 없습니다.");
 		}
-		
-		// 핵심로직 - 기존값 다 들고 있어야함 -> session
+
+		userService.회원수정(principal, dto);
+
+		// 세션 동기화 해주는 부분
 		principal.setEmail(dto.getEmail());
-		session.setAttribute("principal", principal);
-		
-		userRepository.save(principal);
-		
+		session.setAttribute("principal", principal); // 세션 값 변경
+
 		return new CMRespDto<>(1, "성공", null);
 	}
-	
-	
-	// 회원 정보 수정 
-	@GetMapping("/user/{id}") //RestFul API 
+	@GetMapping("/user/{id}")
 	public String userInfo(@PathVariable int id) {
-		// 기본은 userRepository.findById(id) 디비에서 select 해서 가져와야함 -> 모델에 담아가야함
-		// 편법은 세션값을 가져올 수 있다.
-		
+		// 기본은 userRepository.findById(id) 디비에서 가져와야 함.
+		// 편법은 세션값을 가져올 수도 있다.
 		return "user/updateForm";
 	}
-	
 	@GetMapping("/logout")
 	public String logout() {
-	//	session.setAttribute("principal", null ); -> 다른 키 값 안먹음 
-		session.invalidate(); // 세션 무효화 (jsessionId에 있는 값을 비우는것)
-		return "board/list"; //게시글 목록 화면에 데이터 ?? 
+		session.invalidate(); // 세션 무효화 (jsessionId에 있는 값을 비우는 것)
+		return "redirect:/"; // 게시글 목록 화면에 데이터가 있을까요?
 	}
-	
 	@GetMapping("/loginForm")
 	public String loginForm() {
 		return "user/loginForm";
 	}
-	
 	@GetMapping("/joinForm")
 	public String joinForm() {
 		return "user/joinForm";
 	}
-	
 	@PostMapping("/login")
 	public @ResponseBody String login(@Valid LoginReqDto dto, BindingResult bindingResult) {
-		
-		if(bindingResult.hasErrors()) {
+		if (bindingResult.hasErrors()) {
 			Map<String, String> errorMap = new HashMap<>();
-			for(FieldError error : bindingResult.getFieldErrors()) {
+			for (FieldError error : bindingResult.getFieldErrors()) {
 				errorMap.put(error.getField(), error.getDefaultMessage());
 			}
 			return Script.back(errorMap.toString());
 		}
-		User userEntity =
-			userRepository.mLogin(
-					dto.getUsername(), 
-					SHA.encrypt(dto.getPassword(), MyAlgorithm.SHA256)
-				);
-		
-		if(userEntity == null) { // username, password 잘못 기입
+		User userEntity =  userService.로그인(dto);
+		if (userEntity == null) { // username, password 잘못 기입
 			return Script.back("아이디 혹은 비밀번호를 잘못 입력하였습니다.");
-		}else {
-			// 세션 날라가는 조건 : 1. session.invalidate() , 2. 브라우저를 닫으면 날라감 
+		} else {
+			// 세션 날라가는 조건 : 1. session.invalidate(), 2. 브라우저를 닫으면 날라감
 			session.setAttribute("principal", userEntity);
 			return Script.href("/", "로그인 성공");
 		}
 	}
-	
 	@PostMapping("/join")
 	public @ResponseBody String join(@Valid JoinReqDto dto, BindingResult bindingResult) { // username=love&password=1234&email=love@nate.com
-		
-		if(bindingResult.hasErrors()) {
+		if (bindingResult.hasErrors()) {
 			Map<String, String> errorMap = new HashMap<>();
-			for(FieldError error : bindingResult.getFieldErrors()) {
+			for (FieldError error : bindingResult.getFieldErrors()) {
 				errorMap.put(error.getField(), error.getDefaultMessage());
 			}
-
 			return Script.back(errorMap.toString());
-			
 		}
+		userService.회원가입(dto);
 		
-		String encPassword = SHA.encrypt(dto.getPassword(), MyAlgorithm.SHA256);
-		
-		dto.setPassword(encPassword);
-		userRepository.save(dto.toEntity());
 		return Script.href("/loginForm"); // 리다이렉션 (300)
 	}
-	
 }
-
-
-
